f9add077eb3df32386aeeb5dd7b6e730f7fa8a53
Jeremy Freetly <freet8707@gmail.com>
2020-04-17 20:50:13 -0500

    character card component initialized and axios request valid

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import CharacterCard from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
 
+  useEffect(()=>{
+    axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then(response => {
+      console.log(response);      
+      setCharacters(response.data.results)
+    })
+  }, []);
+  console.log(characters);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <h1>Characters</h1>
+      <div>
+        {characters.map(characters =>{
+          return <CharacterCard key={characters.id} charName={characters.name} status={characters.status} species={characters.species} gender={characters.gender} origin={characters.origin.name} image={characters.location.image} />
+        })}        
+      </div>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterCard = (props) => {
+    return (
+        <div>
+            <section>
+                <h2>{props.charName}</h2>
+            </section>
+        </div>
+    )
+}
+
+export default CharacterCard;
